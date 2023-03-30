ea2addd1f6a70d079ed33a034e973213da7658e9
JUAN.ACOSTA <juan.acosta@LATAM-WWFLJJ.local>
2023-03-30 18:46:59 -0500

    new constants (colors,sizes,styles)

---
+++ styles/main.ts
@@ -0,0 +1,23 @@
+export const colors = {
+    primary: "#443C68",
+    secondary: "#393053",
+    dark: "#18122B",
+    light: "#635985",
+    black: "#000",
+    white: "#fff",
+    gray: "#ccc"
+}
+
+export const sizes = {
+    xs: 6,
+    sm: 8,
+    md: 16,
+    lg: 24,
+    xl: 32
+}
+
+export const mainTitle = {
+    fontSize: sizes.lg,
+    color : colors.white,
+    margin : sizes.md
+}
