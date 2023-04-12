cfe9d2e3846c978c7091bfab9fbe408fa8b701c3
JUAN.ACOSTA <juan.acosta@LATAM-WWFLJJ.local>
2023-04-12 17:02:43 -0500

    initial Favorites Screen  added

--- screens/FavoritesScreen.tsx
+++ screens/FavoritesScreen.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import { Text, View } from "react-native"
+import { useSelector } from "react-redux";
+import { favoritesSummonerSelector } from "../redux/slices/FavoritesSummonersSlice";
+
 
 function FavoritesScreen() {
-    return <View><Text>Favorites</Text></View>
+    const { favorites } = useSelector(favoritesSummonerSelector)
+    return <View>
+    
+        <Text>Favorites</Text>
+        {
+            favorites.map((favId) => {
+                return <Text id={favId} >{favId}</Text>
+            })
+        }
+     
+    </View>
 }
 
 export default FavoritesScreen;
