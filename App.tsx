9bca0022a903804de300fe66750bc6955f55b7f2
JuanDiegoAcostaT <juandiegoat01@gmail.com>
2025-02-03 19:35:10 -0500

    ref: code reordered "App"

--- App.tsx
+++ App.tsx
@@ -6,50 +6,56 @@
  */
 
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SearchScreen from './screens/SearchScreen';
 import HomeScreen from './screens/HomeScreen';
 import FavoritesScreen from './screens/FavoritesScreen';
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import LoginScreen from './screens/LogInScreen';
 import HeroeOverviewScreen from './screens/ChampionOverviewScreen';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import SummonerOverviewScreen from './screens/SummonerOverViewScreen';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import {Provider} from 'react-redux';
+import {store} from './redux/store';
 import Spinner from './components/Spinner';
 
 const Tab = createBottomTabNavigator();
 
 const MainTabNavigator = () => {
-  return <Tab.Navigator
-    screenOptions={{
-      headerShown: false
-    }}
-  >
-    <Tab.Screen name="Home" component={HomeScreen} />
-    <Tab.Screen name="Search" component={SearchScreen} />
-    <Tab.Screen name="Favs" component={FavoritesScreen} />
-    <Tab.Screen name="LogIn" component={LoginScreen} />
-  </Tab.Navigator>
-}
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Search" component={SearchScreen} />
+      <Tab.Screen name="Favs" component={FavoritesScreen} />
+      <Tab.Screen name="LogIn" component={LoginScreen} />
+    </Tab.Navigator>
+  );
+};
 
 const Stack: any = createNativeStackNavigator();
 
 function App(): JSX.Element {
   return (
-    <SafeAreaProvider >
+    <SafeAreaProvider>
       <Provider store={store}>
         <NavigationContainer>
           <Stack.Navigator
             screenOptions={{
-              headerShown: false
-            }}
-          >
+              headerShown: false,
+            }}>
             <Stack.Screen name="MainTab" component={MainTabNavigator} />
-            <Stack.Screen name="HeroeOverView" component={HeroeOverviewScreen} />
-            <Stack.Screen name="SummonerOverView" component={SummonerOverviewScreen} />
+            <Stack.Screen
+              name="HeroeOverView"
+              component={HeroeOverviewScreen}
+            />
+            <Stack.Screen
+              name="SummonerOverView"
+              component={SummonerOverviewScreen}
+            />
           </Stack.Navigator>
           <Spinner />
         </NavigationContainer>
@@ -59,5 +65,3 @@ function App(): JSX.Element {
 }
 
 export default App;
-
-
