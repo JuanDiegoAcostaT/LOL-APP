449c92b3b97ba4eda2a095edf1513db334173c16
JuanDiegoAcostaT <juandiegoat01@gmail.com>
2025-02-03 20:08:15 -0500

    ref: code reordered "styles/main"

--- styles/main.ts
+++ styles/main.ts
@@ -1,24 +1,24 @@
 export const colors = {
-    primary: "#443C68",
-    secondary: "#393053",
-    dark: "#18122B",
-    light: "#635985",
-    black: "#000",
-    white: "#fff",
-    gray: "#ccc"
-}
+  primary: '#443C68',
+  secondary: '#393053',
+  dark: '#18122B',
+  light: '#635985',
+  black: '#000',
+  white: '#fff',
+  gray: '#ccc',
+};
 
 export const sizes = {
-    xs: 6,
-    sm: 8,
-    md: 16,
-    lg: 24,
-    xl: 32,
-    xxl : 64
-}
+  xs: 6,
+  sm: 8,
+  md: 16,
+  lg: 24,
+  xl: 32,
+  xxl: 64,
+};
 
 export const mainTitle = {
-    fontSize: sizes.lg,
-    color : colors.white,
-    margin : sizes.md
-}
+  fontSize: sizes.lg,
+  color: colors.white,
+  margin: sizes.md,
+};
