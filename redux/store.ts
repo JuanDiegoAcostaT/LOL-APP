d6f5dea1b31f451a059699b9f690583723623eee
JuanDiegoAcostaT <juandiegoat01@gmail.com>
2025-02-03 20:00:47 -0500

    ref: code reordered "redux/store"

--- redux/store.ts
+++ redux/store.ts
@@ -1,17 +1,16 @@
-import { AnyAction, configureStore, ThunkAction } from "@reduxjs/toolkit";
-import ChampionsSlice from "./slices/ChampionsSlice";
-import FavoritesSummonersSlice from "./slices/FavoritesSummonersSlice";
-import SpinnerSlice from "./slices/SpinnerSlice";
-
+import {AnyAction, configureStore, ThunkAction} from '@reduxjs/toolkit';
+import ChampionsSlice from './slices/ChampionsSlice';
+import FavoritesSummonersSlice from './slices/FavoritesSummonersSlice';
+import SpinnerSlice from './slices/SpinnerSlice';
 
 export type AppThunk = ThunkAction<void, RootState, unknown, AnyAction>;
 
 export const store = configureStore({
-    reducer: {
-        favoritesSummonerStore: FavoritesSummonersSlice,
-        spinnerStore: SpinnerSlice,
-        championsStore : ChampionsSlice
-    },
+  reducer: {
+    favoritesSummonerStore: FavoritesSummonersSlice,
+    spinnerStore: SpinnerSlice,
+    championsStore: ChampionsSlice,
+  },
 });
 
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
