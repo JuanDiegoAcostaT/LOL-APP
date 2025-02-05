109e6b37d1b588a8c1ae895e3fdda4004c7bd676
JuanDiegoAcostaT <juandiegoat01@gmail.com>
2025-02-05 18:08:13 -0500

    ref: code reordered "useSafeArea"

--- hooks/useSafeAre.ts
+++ hooks/useSafeAre.ts
@@ -1,13 +1,13 @@
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 export const useSafeArea = () => {
-    const insets = useSafeAreaInsets();
-    return {
-        insets: {
-            paddingTop: insets.top,
-            // paddingBottom: insets.bottom,
-            paddingLeft: insets.left,
-            paddingRight: insets.right,
-        }
-    }
-}
+  const insets = useSafeAreaInsets();
+  return {
+    insets: {
+      paddingTop: insets.top,
+      // paddingBottom: insets.bottom,
+      paddingLeft: insets.left,
+      paddingRight: insets.right,
+    },
+  };
+};
