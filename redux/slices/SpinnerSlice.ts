9048f7257d9a2901461814596a6d000db69d2c01
JuanDiegoAcostaT <juandiegoat01@gmail.com>
2025-02-05 18:16:39 -0500

    ref: code reordered "spinnerSlice"

--- redux/slices/SpinnerSlice.ts
+++ redux/slices/SpinnerSlice.ts
@@ -1,28 +1,26 @@
-// src / features / photos / PhotoSlice.ts
-
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 
 export interface SpinnerState {
-    active: boolean
+  active: boolean;
 }
 
 const initialState: SpinnerState = {
-    active: false
-}
+  active: false,
+};
 
 const spinnerSlice = createSlice({
-    name: "spinner",
-    initialState,
-    reducers: {
-        toggleActive: (state, { payload }: PayloadAction<any>) => {
-            state.active = payload
-        },
+  name: 'spinner',
+  initialState,
+  reducers: {
+    toggleActive: (state, {payload}: PayloadAction<any>) => {
+      state.active = payload;
     },
-})
+  },
+});
 
-export const { toggleActive } = spinnerSlice.actions
+export const {toggleActive} = spinnerSlice.actions;
 
-export default spinnerSlice.reducer
+export default spinnerSlice.reducer;
 
-export const spinnerActiveSelector = (state: { spinnerStore: SpinnerState }) =>
-    state.spinnerStore.active
+export const spinnerActiveSelector = (state: {spinnerStore: SpinnerState}) =>
+  state.spinnerStore.active;
