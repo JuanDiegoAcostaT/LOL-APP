1e52ede569d731f90697f51fc508b97378f8e30f
JuanDiegoAcostaT <juandiegoat01@gmail.com>
2025-02-05 18:13:35 -0500

    feat: "isUrl" scenario implemented in "ImageContainer"

--- components/ImageContainer.tsx
+++ components/ImageContainer.tsx
@@ -1,22 +1,44 @@
-import { ReactElement, useState } from "react"
-import { Image } from "react-native";
+import React, {ReactElement, useState} from 'react';
+import {Image} from 'react-native';
+//@ts-ignore
+import lolLogo from '../assets/images/lol_logo.png';
 
-function ImageContainer(
-    { mainImage, styles }:
-        { mainImage: string, styles: any }): ReactElement {
+//TODO
+const DEFAULT_IMAGES: any = {
+  lolLogo,
+};
 
-    const [image, setImage] =
-        useState<string>(mainImage)
+function ImageContainer({
+  mainImage,
+  styles,
+  isUrl,
+}: {
+  isUrl?: boolean;
+  mainImage: string;
+  styles: any;
+}): ReactElement {
+  const [image, setImage] = useState<string>(mainImage);
 
-    const resetImage = (): void => {
-        setImage('https://i.imgur.com/AizUHEf.png')
-    }
-
-    return <Image
-        source={{ uri: image }}
+  const resetImage = (): void => {
+    setImage('https://i.imgur.com/AizUHEf.png');
+  };
+  if (isUrl) {
+    return (
+      <Image
+        source={{uri: image}}
         style={styles}
         onError={() => resetImage()}
+      />
+    );
+  }
+
+  return (
+    <Image
+      source={DEFAULT_IMAGES[mainImage]}
+      style={styles}
+      onError={() => resetImage()}
     />
+  );
 }
 
-export default ImageContainer
+export default ImageContainer;
