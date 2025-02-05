53d7e6a4b7d25122c526e1f2d8570fcdc66ebb58
JuanDiegoAcostaT <juandiegoat01@gmail.com>
2025-02-05 18:22:19 -0500

    ref: "BottomDrawer" code reordered

--- components/BottomDrawer.tsx
+++ components/BottomDrawer.tsx
@@ -1,73 +1,64 @@
 import React, {
-    forwardRef,
-    ForwardRefExoticComponent,
-    RefAttributes,
-    useImperativeHandle,
-    useState
-} from "react"
-import { Animated, Dimensions } from "react-native"
+  forwardRef,
+  ForwardRefExoticComponent,
+  RefAttributes,
+  useImperativeHandle,
+  useState,
+} from 'react';
+import {Animated, Dimensions, StyleSheet} from 'react-native';
 
 type IBottomDrawer = {
-    children?: React.ReactNode;
-}
+  children?: React.ReactNode;
+};
 
-const BottomDrawer:
-    ForwardRefExoticComponent<IBottomDrawer & RefAttributes<unknown>>
-    = forwardRef((props: IBottomDrawer, ref: any) => {
+const BottomDrawer: ForwardRefExoticComponent<
+  IBottomDrawer & RefAttributes<unknown>
+> = forwardRef((props: IBottomDrawer, ref: any) => {
+  const {children} = props;
+  const intialState: Animated.Value = new Animated.Value(0);
+  const [state, setState] = useState<Animated.Value>(intialState);
 
-        const { children } = props
-        const intialState : Animated.Value = new Animated.Value(0)
-        const height : number 
-        = Dimensions.get('window').height
+  useImperativeHandle(ref, () => ({
+    startAnimation() {
+      Animated.timing(state, {
+        toValue: -500,
+        duration: 500,
+        useNativeDriver: false,
+      }).start(() => {
+        setState(intialState);
+      });
+    },
 
-        const [state, setState] = useState<Animated.Value>(intialState)
+    endAnimation() {
+      Animated.timing(state, {
+        toValue: 0,
+        duration: 1000,
+        useNativeDriver: false,
+      }).start(() => {
+        setState(intialState);
+      });
+    },
+  }));
 
-        useImperativeHandle(ref, () => ({
+  const animatedStyles = {
+    transform: [{translateY: state}],
+  };
 
-            startAnimation() {
-                Animated.timing(state, {
-                    toValue: -600,
-                    duration: 500,
-                    useNativeDriver: false
-                }).start(() => {
-                    setState(intialState)
-                });
-            },
+  return (
+    <Animated.View style={[styles.animated, animatedStyles]}>
+      {children}
+    </Animated.View>
+  );
+});
 
-            endAnimation() {
-                Animated.timing(state, {
-                    toValue: 10,
-                    duration: 1000,
-                    useNativeDriver: false
-                }).start(() => {
-                    setState(intialState)
-                });
-            }
-        }));
+const styles = StyleSheet.create({
+  animated: {
+    width: '100%',
+    height: Dimensions.get('window').height,
+    position: 'absolute',
+    zIndex: 1,
+    bottom: -(Dimensions.get('window').height * 0.75),
+  },
+});
 
-
-        const animatedStyles = {
-            transform: [
-                { translateY: state }
-            ]
-        }
-
-        return <Animated.View
-            style={[
-                {
-                    width: '100%',
-                    height: height ,
-                    position: 'absolute',
-                    zIndex: 1,
-                    bottom: -height + (height * 0.2),
-                },
-                animatedStyles
-            ]}
-        >
-            {children}
-        </ Animated.View>
-    })
-
-
-
-export default BottomDrawer
+export default BottomDrawer;
