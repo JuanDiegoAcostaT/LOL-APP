77da1e74597c16f52222b7334291bc2126630854
JuanDiegoAcostaT <juandiegoat01@gmail.com>
2025-02-06 22:18:34 -0500

    feat: thunks  "storeFavorites", "fetchFavorites" and "deleteFavorites" implemented in "favoritesSummonerSlice"

--- redux/slices/FavoritesSummonersSlice.ts
+++ redux/slices/FavoritesSummonersSlice.ts
@@ -1,6 +1,10 @@
-// src / features / photos / PhotoSlice.ts
-
-import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {
+  deleteFavs,
+  fetchFavs,
+  storeFavs,
+} from '../../services/FavoritesService';
+import {toggleActive} from './SpinnerSlice';
 
 export interface FavoritesSummonerState {
   favorites: string[];
@@ -9,6 +13,55 @@ export interface FavoritesSummonerState {
 const initialState: FavoritesSummonerState = {
   favorites: [],
 };
+
+//TODO
+export const storeFavorites = createAsyncThunk<any, any, any>(
+  'favorites/storeFavs',
+  async (params, {dispatch}) => {
+    dispatch(toggleActive(true));
+    try {
+      const fav = await storeFavs(params);
+      dispatch(toggleActive(false));
+      return {
+        id: params,
+        key: fav.name,
+      };
+    } catch (err) {
+      console.log(err);
+      dispatch(toggleActive(false));
+    }
+  },
+);
+
+export const fetchFavorites = createAsyncThunk(
+  'favorites/fetchFavs',
+  async (params, {dispatch}) => {
+    dispatch(toggleActive(true));
+    try {
+      const favs = await fetchFavs();
+      dispatch(toggleActive(false));
+      return favs;
+    } catch (err) {
+      console.log(err);
+      dispatch(toggleActive(false));
+    }
+  },
+);
+
+export const deleteFavorites = createAsyncThunk<any, any, any>(
+  'favorites/deleteFavs',
+  async (params, {dispatch}) => {
+    dispatch(toggleActive(true));
+    try {
+      await deleteFavs(params);
+      dispatch(toggleActive(false));
+      return params;
+    } catch (err) {
+      console.log(err);
+      dispatch(toggleActive(false));
+    }
+  },
+);
 
 const favoritesSummonerSlice = createSlice({
   name: 'favoritesSummoner',
@@ -22,6 +75,21 @@ const favoritesSummonerSlice = createSlice({
     setFavs: (state, {payload}: PayloadAction<string>) => {
       state.favorites = [...state.favorites, payload];
     },
+  },
+  extraReducers: builder => {
+    builder.addCase(storeFavorites.fulfilled, (state, action) => {
+      state.favorites = [...state.favorites, action.payload];
+    });
+    builder.addCase(fetchFavorites.fulfilled, (state, action) => {
+      if (action.payload) {
+        state.favorites = action.payload;
+      }
+    });
+    builder.addCase(deleteFavorites.fulfilled, (state, action) => {
+      state.favorites = state.favorites.filter(
+        (fav: any) => fav.key !== action.payload,
+      );
+    });
   },
 });
 
