6f3c7e73fed5ae32be44c1aa1a36098ee93550ce
JuanDiegoAcostaT <juandiegoat01@gmail.com>
2025-02-06 22:06:44 -0500

    ref: code reordered "Spinner"

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -1,27 +1,30 @@
-import React, { ReactElement } from "react";
-import { ActivityIndicator, StyleSheet, View } from "react-native";
-import { useSelector } from "react-redux";
-import { spinnerActiveSelector } from "../redux/slices/SpinnerSlice";
-import { colors } from "../styles/main";
+import React, {ReactElement} from 'react';
+import {ActivityIndicator, StyleSheet, View} from 'react-native';
+import {useSelector} from 'react-redux';
+import {spinnerActiveSelector} from '../redux/slices/SpinnerSlice';
+import {colors} from '../styles/main';
 
 function Spinner(): ReactElement {
-    const active: boolean = useSelector(spinnerActiveSelector)
+  const active: boolean = useSelector(spinnerActiveSelector);
 
-    return <>
-        {
-            active ?
-                <View
-                    style={{
-                        ...(StyleSheet.absoluteFill as object),
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        backgroundColor: 'rgba(68, 60, 104, 0.8)'
-                    }}>
-                    <ActivityIndicator
-                        animating={true}
-                        size={'large'}
-                        color={colors.white} />
-                </View> : null
-        }</>
+  return (
+    <>
+      {active ? (
+        <View
+          style={{
+            ...(StyleSheet.absoluteFill as object),
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'rgba(68, 60, 104, 0.8)',
+          }}>
+          <ActivityIndicator
+            animating={true}
+            size={'large'}
+            color={colors.white}
+          />
+        </View>
+      ) : null}
+    </>
+  );
 }
-export default Spinner
+export default Spinner;
