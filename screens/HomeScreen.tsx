3cd2eba4b82bfffabdb3bd4fb2759b96461aaac5
JuanDiegoAcostaT <juandiegoat01@gmail.com>
2025-02-10 20:48:50 -0500

    ref: code reordered "HomeScreen"

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,47 +1,47 @@
-import React, { ReactElement, useEffect } from "react";
-import { FlatList, Text, View } from "react-native";
-import { useSafeArea } from "../hooks/useSafeAre";
-import { colors, mainTitle } from "../styles/main";
-import Champion from "../components/Champion";
-import { IChampion } from "../interfaces/Champion";
-import { useDispatch, useSelector } from "react-redux";
-import { championsSelector, ChampionsState, fetchChampionsList } from "../redux/slices/ChampionsSlice";
-import { AnyAction, ThunkDispatch } from "@reduxjs/toolkit";
+import React, {ReactElement, useEffect} from 'react';
+import {FlatList, Text, View} from 'react-native';
+import {useSafeArea} from '../hooks/useSafeAre';
+import {colors, mainTitle} from '../styles/main';
+import Champion from '../components/Champion';
+import {IChampion} from '../interfaces/Champion';
+import {useDispatch, useSelector} from 'react-redux';
+import {
+  championsSelector,
+  ChampionsState,
+  fetchChampionsList,
+} from '../redux/slices/ChampionsSlice';
+import {AnyAction, ThunkDispatch} from '@reduxjs/toolkit';
 
-function renderCategoryItem({ item }:
-    { item: IChampion }): ReactElement {
-    return <Champion champion={item} />
+function renderCategoryItem({item}: {item: IChampion}): ReactElement {
+  return <Champion champion={item} />;
 }
 
 function HomeScreen() {
-    const { insets } = useSafeArea()
-    const dispatch = useDispatch<ThunkDispatch<ChampionsState, any, AnyAction>>()
-    const champions = useSelector(championsSelector)
+  const {insets} = useSafeArea();
+  const dispatch = useDispatch<ThunkDispatch<ChampionsState, any, AnyAction>>();
+  const champions = useSelector(championsSelector);
 
-    useEffect(() => {
-        dispatch(fetchChampionsList())
-    }, [dispatch])
+  useEffect(() => {
+    dispatch(fetchChampionsList());
+  }, [dispatch]);
 
-    return <View
-        style={{
-            ...insets,
-            backgroundColor: colors.primary,
-            flex: 1
-        }}
-    >
-        <Text style={mainTitle} >
-            Trending
-        </Text>
+  return (
+    <View
+      style={{
+        ...insets,
+        backgroundColor: colors.primary,
+        flex: 1,
+      }}>
+      <Text style={mainTitle}>Trending</Text>
 
-        <FlatList
-            data={champions}
-            keyExtractor={(item) => item.id}
-            renderItem={renderCategoryItem}
-            numColumns={2}
-        />
+      <FlatList
+        data={champions}
+        keyExtractor={item => item.id}
+        renderItem={renderCategoryItem}
+        numColumns={2}
+      />
     </View>
+  );
 }
 
 export default HomeScreen;
-
-
