90af94f4d77f6052afd600df0932225d534aea98
JuanDiegoAcostaT <juandiegoat01@gmail.com>
2025-02-10 20:11:33 -0500

    ref: code reordered "FavoritesScreen"

--- screens/FavoritesScreen.tsx
+++ screens/FavoritesScreen.tsx
@@ -1,21 +1,18 @@
-import React from "react";
-import { Text, View } from "react-native"
-import { useSelector } from "react-redux";
-import { favoritesSummonerSelector } from "../redux/slices/FavoritesSummonersSlice";
-
+import React from 'react';
+import {Text, View} from 'react-native';
+import {useSelector} from 'react-redux';
+import {favoritesSummonerSelector} from '../redux/slices/FavoritesSummonersSlice';
 
 function FavoritesScreen() {
-    const { favorites } = useSelector(favoritesSummonerSelector)
-    return <View>
-
-        <Text>Favorites</Text>
-        {
-            favorites.map((favId) => {
-                return <Text id={favId} >{favId}</Text>
-            })
-        }
-
+  const {favorites} = useSelector(favoritesSummonerSelector);
+  return (
+    <View>
+      <Text>Favorites</Text>
+      {favorites.map(favId => {
+        return <Text id={favId}>{favId}</Text>;
+      })}
     </View>
+  );
 }
 
 export default FavoritesScreen;
