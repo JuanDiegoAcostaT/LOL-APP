a9ea2b2460be0a8d4725f4e5f5f327036af4eb06
JuanDiegoAcostaT <juandiegoat01@gmail.com>
2025-02-10 20:13:18 -0500

    feat: component "FlatList" implemented in "FavoritesScreen"

--- screens/FavoritesScreen.tsx
+++ screens/FavoritesScreen.tsx
@@ -1,18 +1,49 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {ReactElement, useMemo} from 'react';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 import {useSelector} from 'react-redux';
+import Champion from '../components/Champion';
+import {useSafeArea} from '../hooks/useSafeAre';
+import {IChampion} from '../interfaces/Champion';
+import {championsSelector} from '../redux/slices/ChampionsSlice';
 import {favoritesSummonerSelector} from '../redux/slices/FavoritesSummonersSlice';
+import {colors, mainStyles} from '../styles/main';
+
+function renderCategoryItem({item}: {item: IChampion}): ReactElement {
+  return <Champion view={'favs'} champion={item} />;
+}
 
 function FavoritesScreen() {
   const {favorites} = useSelector(favoritesSummonerSelector);
+  const champions = useSelector(championsSelector);
+  const {insets} = useSafeArea();
+
+  const favChamps = useMemo(() => {
+    if (champions) {
+      return champions?.filter((champ: IChampion) => {
+        return favorites.findIndex((fav: any) => champ.id === fav.id) > -1;
+      });
+    } else {
+      return [];
+    }
+  }, [champions, favorites]);
+
   return (
-    <View>
-      <Text>Favorites</Text>
-      {favorites.map(favId => {
-        return <Text id={favId}>{favId}</Text>;
-      })}
+    <View style={[insets, styles.favoritesContainer]}>
+      <Text style={mainStyles.mainTitle}>Favorites</Text>
+
+      <FlatList
+        data={favChamps}
+        keyExtractor={item => item.id}
+        renderItem={renderCategoryItem}
+        numColumns={2}
+      />
     </View>
   );
 }
-
+const styles = StyleSheet.create({
+  favoritesContainer: {
+    flex: 1,
+    backgroundColor: colors.secondary,
+  },
+});
 export default FavoritesScreen;
