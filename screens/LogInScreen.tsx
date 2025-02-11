041aeb3cdcf920d176c8dd96c4b5fc46afb5f909
JuanDiegoAcostaT <juandiegoat01@gmail.com>
2025-02-10 20:14:21 -0500

    feat: "AuthForm" implemented in "LoginScreen"

--- screens/LogInScreen.tsx
+++ screens/LogInScreen.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {ReactElement} from 'react';
+import AuthForm from '../components/AuthForm';
 
-function LoginScreen() {
-  return (
-    <View>
-      <Text>Login</Text>
-    </View>
-  );
+function LoginScreen(): ReactElement {
+  return <AuthForm isLogin={true} />;
 }
 
 export default LoginScreen;
